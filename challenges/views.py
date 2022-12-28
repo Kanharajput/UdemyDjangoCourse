23e636900ec427015625509f5d6de915f3ec6df7
Kanha Tomar <kanharajput91@gmail.com>
2022-12-28 14:00:07 +0530

    another url and view

--- challenges/views.py
+++ challenges/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
+def jan(request):
     return HttpResponse("kanha is a great man")
+
+def feb(request):
+    return HttpResponse("Mohit ki shadi")
