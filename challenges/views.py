a792fad58f2ed2adc14d04fd6e4370c20682c448
Kanha Tomar <kanharajput91@gmail.com>
2022-12-29 11:44:00 +0530

    redirect url

--- challenges/views.py
+++ challenges/views.py
@@ -1,19 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
 '''
 def january(request):
     return HttpResponse("Finish udemy")
 '''
-# handling url which have number
-def monthlyChallengeWithNumber(request,month):
-    if month == 1:
-        return HttpResponse("1")
-
-    else:
-        return HttpResponseNotFound("Url not found")
-
 # all month challenges it like an switch statement as python doesn't have switch
 # it prevent us from typing different if condition for all months
 all_challenges = {
@@ -26,17 +18,31 @@ all_challenges = {
     "july" : "Practise of course",
     "august" : "course take 5 months and it is 3rd",
     "september" : "complete previous learnt things",
-    "octomber" : "last month of course",
-    "novermber" : "Some projects from companies",
+    "october" : "last month of course",
+    "november" : "Some projects from companies",
     "december" : "Practise and enjoy placement after 8 sem that'swhy",
 }
+
+
+# handling url which have number
+def monthlyChallengeWithNumber(request,month):
+    all_challenges_keys_list = list(all_challenges.keys())        # gettings keys like january, february
+    
+    # allow till 12 only as size of list is 12
+    if month <= len(all_challenges_keys_list):
+        month_related_to_number = all_challenges_keys_list[month-1]      # getting month name with number, used as index
+        return HttpResponseRedirect("/challenges/" + month_related_to_number)       # redirecting to the actual url
+
+    return HttpResponseNotFound("There are no months greater than 12")
+
+
+
 
 # handling many urls 
 # getting month variable from the url
 def monthlyChallenge(request, month):
     try:
         challenge = all_challenges[month]
-        print(challenge)
         return HttpResponse(challenge)
 
     except:
