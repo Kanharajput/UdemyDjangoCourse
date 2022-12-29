750f420c1b9994dc2f2effd60edddc6bbba5d024
Kanha Tomar <kanharajput91@gmail.com>
2022-12-29 11:07:19 +0530

    challenges set for all months

--- challenges/views.py
+++ challenges/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse, HttpResponseNotFound
 def january(request):
     return HttpResponse("Finish udemy")
 '''
-
+# handling url which have number
 def monthlyChallengeWithNumber(request,month):
     if month == 1:
         return HttpResponse("1")
@@ -14,22 +14,30 @@ def monthlyChallengeWithNumber(request,month):
     else:
         return HttpResponseNotFound("Url not found")
 
+# all month challenges it like an switch statement as python doesn't have switch
+# it prevent us from typing different if condition for all months
+all_challenges = {
+    "january" : "Finish udemy course",
+    "february" : "Socha nahi",
+    "march" : "Attend marriages",
+    "april" : "Exam prepration",
+    "may" : "Write exam",
+    "june" : "Enroll a course",
+    "july" : "Practise of course",
+    "august" : "course take 5 months and it is 3rd",
+    "september" : "complete previous learnt things",
+    "octomber" : "last month of course",
+    "novermber" : "Some projects from companies",
+    "december" : "Practise and enjoy placement after 8 sem that'swhy",
+}
 
 # handling many urls 
 # getting month variable from the url
 def monthlyChallenge(request, month):
-    challenge = None
+    try:
+        challenge = all_challenges[month]
+        print(challenge)
+        return HttpResponse(challenge)
 
-    if month == 'january':
-        challenge = "Finish udemy course"
-
-    elif month == 'february':
-        challenge = "Socha nahi"
-
-    elif month == 'march':
-        challenge = "Attend marriages"
-
-    else:
-        return HttpResponseNotFound("Url not found")
-        
-    return HttpResponse(challenge)
+    except:
+        return HttpResponseNotFound("Url doesn't exist")
