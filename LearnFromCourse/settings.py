1708c226264d6c7e160497707fa921645990b509
Kanha Tomar <kanharajput91@gmail.com>
2022-12-31 19:18:56 +0530

    static files for a particular app

--- LearnFromCourse/settings.py
+++ LearnFromCourse/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',         # check this, it is used to manage static files
 ]
 
 MIDDLEWARE = [
@@ -127,6 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+# it is need to change at a time of deployment not now
 STATIC_URL = '/static/'
 
 # Default primary key field type
